1a9288e029dc5de11c651b129b6aefc881532e36
hinoneko <yanasiryk35@gmail.com>
2024-01-14 00:25:42 +0200

    Format code indentation and spacing Remove empty blocks Add 'use strict';

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,13 +1,20 @@
 // Generate random password
 
+// Step 1
+// Format code indentation and spacing
+// Remove empty blocks
+// Add 'use strict';
+
+'use strict';
+
 let GeneratePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
-  key = '';
-  for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
-  }
-  return key;
+    const MAX = alphabet.length;
+    let key = '';
+    for (let i = 0; i < length; i++) {
+        let Index = Math.floor(Math.random() * MAX);
+        key = key + alphabet[Index];
+    }
+    return key;
 };
 
 module.exports = GeneratePassword;

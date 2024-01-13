9e998cbfa810285d46ab53e669544b474514ad34
hinoneko <yanasiryk35@gmail.com>
2024-01-13 23:54:24 +0200

    Format code indentation and spacing Add 'use strict';

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,20 @@
 // Return an array without duplicates
 
+// Step 1
+// Format code indentation and spacing
+// Add 'use strict';
+
+'use strict';
+
 duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
+    if (N <= 0) return [];
+    else {
+        res = [];
+        for (let i = 0; i < N; i++) {
+            res[i] = value;
+        }
+        return res;
     }
-    return res;
-  }
 };
 
 module.exports = duplicate;

04a1c97fe49fda8e857031500a6707ff5563255d
hinoneko <yanasiryk35@gmail.com>
2024-01-13 23:42:24 +0200

    Remove senseless blocks Change identifiers case Add `const` and `let`

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,7 +1,7 @@
 // Get day number
 
-// Step 2
-// Remove unused identifiers
+// Step 3 (final)
+// Remove senseless blocks
 // Change identifiers case
 // Add `const` and `let`
 
@@ -9,10 +9,9 @@
 
 const DAYS_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const parseDay = (s) => {
-    let i;
-    for (i = 0; i < DAYS_OF_WEEK.length; i++) {
-        if (s.startsWith(DAYS_OF_WEEK[i].toLowerCase())) {
+const parseDay = (str) => {
+    for (let i = 0; i < DAYS_OF_WEEK.length; i++) {
+        if (str.startsWith(DAYS_OF_WEEK[i].toLowerCase())) {
             return i + 1;
         }
     }

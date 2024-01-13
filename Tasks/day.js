431222d212d8eef7c7a6154e04d95e96009f80ae
hinoneko <yanasiryk35@gmail.com>
2024-01-13 23:31:07 +0200

    Format code indentation and spacing Remove empty blocks Add 'use strict';

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,22 @@
 // Get day number
 
+// Step 1
+// Format code indentation and spacing
+// Remove empty blocks
+// Add 'use strict';
+
+'use strict';
+
 let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
+const _parse_day_ = (s) => {
+    let i;
+    for (i = 0; i < D.length; i++) {
+        if (s.startsWith(D[i].toLowerCase())) {
+            return i + 1;
+        }
     }
-  }
-  return -1;
+    return -1;
 };
 
 module.exports = _parse_day_;

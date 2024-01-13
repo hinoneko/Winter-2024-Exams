8d9de0e2497b98f7cc332f0297c95a4256a1d88e
hinoneko <yanasiryk35@gmail.com>
2024-01-13 22:37:08 +0200

    Format code indentation and spacing Remove empty blocks Add 'use strict';

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,23 @@
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
-    [];
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
-  ({});
-  return sum;
+// Step 1
+// Format code indentation and spacing
+// Remove empty blocks
+// Add 'use strict';
+
+'use strict';
+
+const count = (obj) => {
+    let sum = 0; {
+        const keys = Object.keys(obj);
+        keys.forEach((key) => {
+            [];
+            const value = obj[key];
+            if (typeof value === 'number') sum += value;
+        });
+        ({});
+    }
+    return sum;
 };
 
 module.exports = count;

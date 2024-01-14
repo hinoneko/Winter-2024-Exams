ace48fce20f54645a49f268414c20406f7df885b
hinoneko <yanasiryk35@gmail.com>
2024-01-14 21:10:08 +0200

    Format code indentation and spacing Change function name to camelCase

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,8 +1,12 @@
 // Get one random element from an array
 
-const SAMPLE = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
+// Step 1
+// Format code indentation and spacing
+// Change function name to camelCase
+
+const getRandomElement = (arr) => {
+    arr = arr[Math.floor(Math.random() * arr.length)];
+    return arr;
 };
 
-module.exports = SAMPLE;
+module.exports = getRandomElement;

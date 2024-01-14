0c7fed915ee1282c5dc8863efd7c9ccb2226f740
hinoneko <yanasiryk35@gmail.com>
2024-01-14 20:46:49 +0200

    Simplify the function by directly using the filter method

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,16 +1,12 @@
 // Filter array by type name
 
-// Step 2
-// Utilize the map and filter methods to achieve the same result more concisely
+// Step 3 (final)
+// Simplify the function by directly using the filter method
 
 'use strict';
 
 const filterArrayByType = (inputArray, typeName) => {
-    const indicesToRemove = inputArray
-        .map((element, index) => (typeof element !== typeName ? index : -1))
-        .filter((index) => index !== -1);
-
-    inputArray = inputArray.filter((element, index) => !indicesToRemove.includes(index));
-
-    return inputArray;
+    return inputArray.filter((element) => typeof element === typeName);
 };
+
+module.exports = filterArrayByType;

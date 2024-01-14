d08eebf111a4b840a8a73207994d0d037367b43e
hinoneko <yanasiryk35@gmail.com>
2024-01-14 22:34:33 +0200

    Remove (...restVariables) Change the loop variable name

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,12 +1,12 @@
 // Increment all numbers in dictionary
 
-// Step 2
-// Improve variable names for better readability
-// Avoid using delete as a loop variable name
+// Step 3 (final)
+// Remove unused parameters and variables (...restVariables)
+// Change the loop variable name to a more descriptive one
 
 'use strict';
 
-const incrementNumbers = (inputObject, ...restVariables) => {
+const incrementNumbers = (inputObject) => {
     for (const key in inputObject) {
         if (typeof inputObject[key] === 'number') {
             inputObject[key] += 1;
